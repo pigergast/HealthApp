88700146f34cc83a666c241de2014298fdd3b8d3
Oggy <vip12345k@gmail.com>
2022-11-09 22:51:05 +0700

    some more stuff done

--- app/src/main/java/com/example/healthapp/BMICalculator.java
+++ app/src/main/java/com/example/healthapp/BMICalculator.java
@@ -3,6 +3,11 @@ package com.example.healthapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class BMICalculator extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class BMICalculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmicalculator);
+        Button calculate = (Button) findViewById(R.id.calculate);
+        EditText height = (EditText) findViewById(R.id.height);
+        EditText weight = (EditText) findViewById(R.id.weight);
+        TextView BMI = (TextView) findViewById(R.id.BMI);
+        TextView status = (TextView) findViewById(R.id.Status);
+        TextView heightText = (TextView) findViewById(R.id.heightText) ;
+        TextView weightText = (TextView) findViewById(R.id.weightText);
+        Switch unit = (Switch) findViewById(R.id.switchUnit);
+        unit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Switch btn = (Switch) view;
+                if(btn.isChecked()){
+                    heightText.setText("Your Height(In):");
+                    weightText.setText("Your Weight(Lb):");
+                }
+                else {
+                    heightText.setText("Your Height(Cm):");
+                    weightText.setText("Your Weight(Kg):");
+                }
+            }
+        });
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String hString = height.getText().toString();
+                String wString = weight.getText().toString();
+            }
+        });
     }
 }
